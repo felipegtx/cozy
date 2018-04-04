d0ddb55c01243ddd667a20cf069886ea3e240f2e
Felipe <felipe.g.te@gmail.com>
2018-04-04 07:43:01 -0300

    Swagger auto generated server

---
+++ out/swagger-server/controllers/Default.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Default = require('../service/DefaultService');
+
+module.exports.getDesignByUserId = function getDesignByUserId (req, res, next) {
+  var user_id = req.swagger.params['user_id'].value;
+  var budget = req.swagger.params['budget'].value;
+  var room_type = req.swagger.params['room_type'].value;
+  var limit = req.swagger.params['limit'].value;
+  Default.getDesignByUserId(user_id,budget,room_type,limit)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
--- out/test.js
+++
@@ -1,5 +0,0 @@
-function A() {
-}
-class Teste {
-}
-//# sourceMappingURL=test.js.map
